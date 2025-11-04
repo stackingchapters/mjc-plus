6a0c6c3bdf5710104e01ec6bdebb11787dc65f7c
jinhengpang <pangjinheng58@gmail.com>
2025-11-04 13:49:11 +1100

    Add Mjc logo to authentication page

--- front-end/src/app/authentication/sign-up/page.tsx
+++ front-end/src/app/authentication/sign-up/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Header, Divider, ThirdPartyAuth, AuthForm } from "../_components";
+import SvgMjcLogo from "@/src/_components/svgs/MjcLogo";
 
 const Login = () => {
   const MODE = "Login";
   return (
-    <div className="flex h-full w-full items-center justify-center bg-[#f5f4f7]">
+    <div className="flex-col h-full w-full items-center bg-[#f5f4f7]">
+      {/* ==== MJC Logo ==== */}
+      <SvgMjcLogo className="size-1/10 transform translate-y-1/2" />
       {/* ==== Card ==== */}
-      <div className="card w-xs flex-col items-center bg-white sm:w-md">
+      <div className="card w-xs flex-col items-center bg-white sm:w-lg">
         {/* ==== Content ==== */}
         <div className="w-4/5 flex-col items-center gap-3 sm:w-3/5">
           <Header mode={MODE} />
