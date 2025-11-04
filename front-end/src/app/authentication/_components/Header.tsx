3da9aae341b539bddc260935a1ad0877d9813ae9
jinhengpang <pangjinheng58@gmail.com>
2025-11-04 13:49:42 +1100

    Give space for the Mjc logo and accentuate header

--- front-end/src/app/authentication/_components/Header.tsx
+++ front-end/src/app/authentication/_components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { AuthMode } from "../types";
 
 const Header = ({ mode }: AuthMode) => {
-  return <h2 className="text-2xl font-medium">{mode}</h2>;
+  return <h2 className="text-2xl font-bold mt-8 mb-3">{mode}</h2>;
 };
 
 export default Header;
