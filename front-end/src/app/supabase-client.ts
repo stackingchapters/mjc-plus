2f3460451424a690b4fb5dcf8fad84d1122b2f23
jinhengpang <pangjinheng58@gmail.com>
2025-12-01 13:04:52 +1100

    Add error handling for retrieving supabase env

--- front-end/src/app/supabase-client.ts
+++ front-end/src/app/supabase-client.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_API) {
+  throw new Error("Missing Supabase environment variables");
+}
+
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_API!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_API
 );
